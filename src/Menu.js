08ffb372c6579de6f1b7313b4b90bd8b10e6e03e
UzomaAkpala <francisakpala@yahoo.com>
2023-11-16 14:55:25 +0100

    Conditional rendering with Ternaries

--- src/Menu.js
+++ src/Menu.js
@@ -1,69 +1,76 @@
 import Pizza from "./Pizza";
 
 const pizzaData = [
-  {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
-    price: 6,
-    photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
-    price: 10,
-    photoName: "pizzas/margherita.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-    price: 12,
-    photoName: "pizzas/spinaci.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
-    price: 12,
-    photoName: "pizzas/funghi.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
-    price: 15,
-    photoName: "pizzas/salamino.jpg",
-    soldOut: true,
-  },
-  {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-    price: 18,
-    photoName: "pizzas/prosciutto.jpg",
-    soldOut: false,
-  },
+  //   {
+  //     name: "Focaccia",
+  //     ingredients: "Bread with italian olive oil and rosemary",
+  //     price: 6,
+  //     photoName: "pizzas/focaccia.jpg",
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: "Pizza Margherita",
+  //     ingredients: "Tomato and mozarella",
+  //     price: 10,
+  //     photoName: "pizzas/margherita.jpg",
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: "Pizza Spinaci",
+  //     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+  //     price: 12,
+  //     photoName: "pizzas/spinaci.jpg",
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: "Pizza Funghi",
+  //     ingredients: "Tomato, mozarella, mushrooms, and onion",
+  //     price: 12,
+  //     photoName: "pizzas/funghi.jpg",
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: "Pizza Salamino",
+  //     ingredients: "Tomato, mozarella, and pepperoni",
+  //     price: 15,
+  //     photoName: "pizzas/salamino.jpg",
+  //     soldOut: true,
+  //   },
+  //   {
+  //     name: "Pizza Prosciutto",
+  //     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+  //     price: 18,
+  //     photoName: "pizzas/prosciutto.jpg",
+  //     soldOut: false,
+  //   },
 ];
 
 function Menu() {
+  const pizza = pizzaData;
+
+  const numPizzas = pizza.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => {
-          return (
-            <Pizza
-              name={pizza.name}
-              key={pizza.name}
-              alt={pizza.name}
-              ingredients={pizza.ingredients}
-              photoName={pizza.photoName}
-              price={pizza.price}
-            />
-          );
-        })}
-      </ul>
+      {numPizzas ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => {
+            return (
+              <Pizza
+                name={pizza.name}
+                key={pizza.name}
+                alt={pizza.name}
+                ingredients={pizza.ingredients}
+                photoName={pizza.photoName}
+                price={pizza.price}
+              />
+            );
+          })}
+        </ul>
+      ) : (
+        <h2>We are working on our Menu, Please check back later</h2>
+      )}
 
       {/* <Pizza
         name="Pizza Spinachi"
