0f3e60efa4a992d6e5369c37c220c11ee41d9936
UzomaAkpala <francisakpala@yahoo.com>
2023-11-16 12:11:26 +0100

    Time logic in footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,16 @@
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closingHour = 22;
+  const isOpen = hour >= openHour && hour <= closingHour;
   return (
-    <footer>{new Date().toLocaleTimeString()}. We are currently open!</footer>
+    <footer>
+      {isOpen ? (
+        <h2>We are currently open</h2>
+      ) : (
+        <h2>We are currently Closed!</h2>
+      )}
+    </footer>
   );
 }
 
