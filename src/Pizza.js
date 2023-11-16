b117293ae938e6a738888236e13b4d867ea1f656
UzomaAkpala <francisakpala@yahoo.com>
2023-11-16 15:58:35 +0100

    setting classes and text conditionally

--- src/Pizza.js
+++ src/Pizza.js
@@ -1,15 +1,13 @@
 function Pizza(props) {
   // only use if/else and switch statements outside the component return logic.
-  if (props.soldOut) return null;
+  //if (props.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${props.soldOut ? "sold-out" : ""}`}>
       <img src={props.photoName} alt={props.alt} />
       <div>
         <h3>{props.name}</h3>
         <p>{props.ingredients}</p>
-        <span>
-          <p>{props.price}</p>
-        </span>
+        <span>{props.soldOut ? "SOLD OUT" : props.price}</span>
       </div>
     </li>
   );
